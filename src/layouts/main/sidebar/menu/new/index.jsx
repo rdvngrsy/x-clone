0204c5a6effce969cbe1b74a23857423d8e7e5cd
Rıdvan Gürsoy <ridvangursoy@hotmail.com>
2024-03-27 19:59:47 +0300

    Sidebar menüleri tamamlandı.

---
+++ src/layouts/main/sidebar/menu/new/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "../../../../../components/button";
+
+const New = () => {
+  return (
+    <div className="py-4 w-[90%]">
+      <Button size={"large"} children={"Gönder"} />
+    </div>
+  );
+};
+
+export default New;
