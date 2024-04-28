54d5a8709198700b7bca8bb0521777ca6c51dd09
Rıdvan Gürsoy <ridvangursoy@hotmail.com>
2024-04-28 21:21:35 +0300

    Right Bar' a başlandı.

--- src/layouts/main/rightbar/index.jsx
+++ src/layouts/main/rightbar/index.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import Search from "./search";
 
 const RightBar = () => {
-  return <aside className="w-[350px] ">RightBar</aside>;
+  return <aside className="w-[350px] ">
+    <Search />
+  </aside>;
 };
 
 export default RightBar;
